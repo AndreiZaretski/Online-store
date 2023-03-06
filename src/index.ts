7cbdfeb2f2c38b8049129ac307c222a8b0b9f098
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-03-06 11:46:31 +0300

    refactor: refactor reviewer comments1

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import "./assets/images/visacard.png";
 import "./assets/images/amexcard.png";
 import "./assets/images/mastercard.png";
 import App from "./pages/app";
-import SortProducts from "./pages/products/sortProducts";
+import ProductsSorting from "./pages/products/sortProducts";
 
 const app = new App();
 app.run();
@@ -15,7 +15,7 @@ window.addEventListener("load", (): void => {
   const catalog: HTMLElement | null = document.querySelector(".catalog");
   const filter: HTMLElement | null = document.querySelector(".filter");
   if (catalog instanceof HTMLElement && filter instanceof HTMLElement) {
-    const writer = new SortProducts(catalog, filter);
+    const writer = new ProductsSorting(catalog, filter);
     writer.writeResSearch(3);
   }
 });
